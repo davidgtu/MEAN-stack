3d243abfcb7b2973e66611cbe21f9efc0e0b59e2
spacebear5000 <david.giahuy.tu@gmail.com>
2015-12-16 16:05:12 -0500

    add ability to create custom posts

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ app.controller('MainController',[
 function($scope){
   $scope.test = "HIYO";
   $scope.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'
-  ]
+    {title: 'post 1', upvotes: 4}, 
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 18},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 5}
+  ];
+  $scope.addPost = function(){
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
 }]);
