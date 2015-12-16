a75d184556a336a755bf69a5bb1969f29746f573
spacebear5000 <david.giahuy.tu@gmail.com>
2015-12-16 15:49:52 -0500

    add test posts

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ app.controller('MainController',[
 '$scope',
 function($scope){
   $scope.test = "HIYO";
+  $scope.posts = [
+    'post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5'
+  ]
 }]);
